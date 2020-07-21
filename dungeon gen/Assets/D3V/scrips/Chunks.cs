6889c955fb064dc2de9972119b2759b16fe6ceca
Siro <siro.knubel@gmail.com>
2020-07-21 04:45:33 +0200

    basic chunk system
    its a little bit from a tutorial but it works

---
+++ dungeon gen/Assets/D3V/scrips/Chunks.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunks : MonoBehaviour
+{
+
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    bool[,,] voxelMap = new bool[VoxelD.ChunkWidth, VoxelD.Chunkhight, VoxelD.ChunkWidth];
+
+
+
+
+    void Start()
+    {
+
+        PopulateVoxelMap();
+        CreateChunkMesh();
+        madeMesh();
+
+
+
+    }
+
+    void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelD.Chunkhight; y++)
+        {
+
+
+            for (int x = 0; x < VoxelD.ChunkWidth; x++)
+            {
+
+
+                for (int z = 0; z < VoxelD.ChunkWidth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+
+
+
+                }
+            }
+        }
+    }
+
+    void CreateChunkMesh()
+    {
+
+        for (int y = 0; y < VoxelD.Chunkhight; y++)
+        {
+
+
+            for (int x = 0; x < VoxelD.ChunkWidth; x++)
+            {
+
+
+                for (int z = 0; z < VoxelD.ChunkWidth; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+
+
+
+
+                }
+            }
+        }
+    }
+
+    bool CheckVoxel (Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelD.ChunkWidth - 1 || y < 0 || y > VoxelD.Chunkhight - 1 || z < 0 || z > VoxelD.ChunkWidth - 1)
+            return false;
+
+
+
+        return voxelMap[x, y, z];
+
+    }
+
+
+
+
+
+    void AddVoxelDataToChunk( Vector3 pos)
+    {
+        for (int p = 0; p < 6; p++)
+        {
+
+            if (!CheckVoxel(pos + VoxelD.faceChecks[p])) { 
+
+            for (int i = 0; i < 6; i++)
+            {
+
+
+
+                int dreieckverzeichnis = VoxelD.voxelDreieck[p, i];
+                vertices.Add(VoxelD.voxelPoints[dreieckverzeichnis] + pos);
+                triangles.Add(vertexIndex);
+
+                uvs.Add(Vector2.zero);
+
+
+                vertexIndex++;
+
+
+            }
+        }
+        }
+    }
+
+    void madeMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+
+        mesh.uv = uvs.ToArray();
+
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+
+}
--- dungeon gen/Assets/Scripts/MenuManager.cs
+++ dungeon gen/Assets/Scripts/MenuManager.cs
@@ -1,37 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
 
-
-    public int G;
-
+    public GameObject C;
+    public GameObject mc;
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 144;
     }
 
-    private void FixedUpdate()
-    {
-        
-    }
 
     // Update is called once per frame
 
     public void loadVoxel3D()
     {
-        SceneManager.LoadScene("3DV", LoadSceneMode.Additive);
+       
+
+        SceneManager.LoadScene("3DV");
     }
     public void loadVoxel2D()
     {
-        SceneManager.LoadScene("3DV", LoadSceneMode.Additive);
+        
+
+        SceneManager.LoadScene("2DV");
     }
     public void loadDCG()
     {
-        SceneManager.LoadScene("3DV", LoadSceneMode.Additive);
+        
+
+        SceneManager.LoadScene("DCG");
     }
 }
