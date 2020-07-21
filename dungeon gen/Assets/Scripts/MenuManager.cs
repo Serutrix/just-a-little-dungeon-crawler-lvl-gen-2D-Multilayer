3d49ad3e0e49d2025dd2cadd98040eaf5fc0bcd6
Siro <siro.knubel@gmail.com>
2020-07-21 02:59:03 +0200

    Menu

---
+++ dungeon gen/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+
+    public int G;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 144;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    public void loadVoxel3D()
+    {
+        SceneManager.LoadScene("3DV", LoadSceneMode.Additive);
+    }
+    public void loadVoxel2D()
+    {
+        SceneManager.LoadScene("3DV", LoadSceneMode.Additive);
+    }
+    public void loadDCG()
+    {
+        SceneManager.LoadScene("3DV", LoadSceneMode.Additive);
+    }
+}
